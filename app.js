efb2a0f6ae4ed6f12e3036ae958988cd6b8d4f5a
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-05-18 23:52:01 +0600

    Install json web token

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//json web token
+const jwt = require('jsonwebtoken')
+const posts = [
+    {
+        username: 'Kabirul',
+        title:'Post 1'
+    },
+    {
+        username: 'Hossain',
+        title: 'Post 2'
+    }
+]
+//Youtube link: 
 app.get('/', (req, res)=>{
     res.send("JWT");
+})
+
+app.get('/posts', (req, res)=>{
+    res.json(posts)
 })
 
 app.listen(port, ()=>{
