9091f0fdb2928cf54a4caf77b2de64df7de0bb92
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-05-19 23:14:23 +0600

    Install dotenv

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = 3000;
 
 //json web token
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+
+app.use(express.json())
 const posts = [
     {
         username: 'Kabirul',
@@ -21,6 +24,14 @@ app.get('/', (req, res)=>{
 
 app.get('/posts', (req, res)=>{
     res.json(posts)
+})
+
+app.post('/login', (req, res)=>{
+    //Authenticate user
+    const username = req.body.username;
+    const user = {name: username};
+    jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    console.log(process.env.ACCESS_TOKEN_SECRET);
 })
 
 app.listen(port, ()=>{
