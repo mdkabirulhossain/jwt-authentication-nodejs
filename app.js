8c60eb4d60a7650b018299f9cc4da41322eb5079
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-05-20 23:28:59 +0600

    Write authenticateToken Function

--- app.js
+++ app.js
@@ -22,18 +22,33 @@ app.get('/', (req, res)=>{
     res.send("JWT");
 })
 
-app.get('/posts', (req, res)=>{
-    res.json(posts)
+app.get('/posts', authenticateToken, (req, res)=>{
+    res.json(posts.filter(post => post.username === req.user.name))
 })
 
 app.post('/login', (req, res)=>{
     //Authenticate user
     const username = req.body.username;
     const user = {name: username};
-    jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
     console.log(process.env.ACCESS_TOKEN_SECRET);
+    res.json({accessToken: accessToken})
+    
 })
 
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split('')[1]
+    if(token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
+        if(err){
+            req.user = user
+            next()
+        }
+    })
+    }
+
 app.listen(port, ()=>{
-    console.log(`Server on running http://localhost:${port}`);
+   console.log(`Server on running http://localhost:${port}`);
 })
